ea8e9f04b7210176f35a370f5aaf496c9921f13e
Barry Gilbert <softwareninja1@gmail.com>
2019-11-20 05:40:36 -0500

    Fixing warning about key

--- src/components/organisms/Puzzle/index.js
+++ src/components/organisms/Puzzle/index.js
@@ -182,7 +182,7 @@ export default class Puzzle extends React.Component {
           />
           {winner && <div>Winner!!!</div>}
           {puzzle.map(row => (
-            <StyledRow>
+            <StyledRow key={row.join(',')}>
               {row.map(piece => (
                 <PuzzlePiece
                   piece={piece}
